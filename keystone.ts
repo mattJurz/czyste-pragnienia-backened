37a86e19cc153fcbf9354b15c63666cbb12a8b9f
mattJurz <jurzyna@gmail.com>
2022-05-09 09:41:36 +0200

    added port to the config

--- keystone.ts
+++ keystone.ts
@@ -6,15 +6,17 @@ const DATABASE_URL =
   process.env.DATABASE_URL ||
   `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@localhost:${process.env.POSTGRESS_PORT}/${process.env.POSTGRESS_DB_NAME}`;
 
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 export default withAuth(
   config({
     db: { provider: "postgresql", url: DATABASE_URL },
-    // server: {
-    //   cors: {
-    //     origin: process.env.FRONTEND_URL,
-    //     credentials: true,
-    //   },
-    // },
+    server: {
+      port: PORT,
+      //   cors: {
+      //     origin: process.env.FRONTEND_URL,
+      //     credentials: true,
+      //   },
+    },
     ui: {
       // TODO: change this for roles
       isAccessAllowed: (context) => !!context.session?.data,
